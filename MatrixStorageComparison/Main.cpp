fc2b5d1c7d5335a19fb73e0b4abd7a0e18723e4d
vitaliivoroshilov <vitallizer@yandex.ru>
2023-10-28 23:50:34 +0300

    .

--- MatrixStorageComparison/Main.cpp
+++ MatrixStorageComparison/Main.cpp
@@ -1,11 +1,12 @@
 #include "CoordinateMatrix.h"
+#include <chrono>
 
 int main()
 {
 	int n1, m1, n2, m2;
 
 	cout << "n1: ";
-	cin >> n1;
+	cin >> n1; 
 	cout << "m1: ";
 	cin >> m1;
 	cout << endl;
@@ -23,11 +24,27 @@ int main()
 	cin >> oneArrMatr2;
 	cout << endl;
 
-	cout << "OneArrayMult:" << endl << oneArrMatr1.OneArrayMult(oneArrMatr2);
+	OneArrayMatrix oneArrRes(n1, m2);
 
-	
+	auto start1 = chrono::high_resolution_clock::now();
+	oneArrRes = oneArrMatr1.OneArrayMult(oneArrMatr2);
+	auto end1 = chrono::high_resolution_clock::now();
+	chrono::duration<double> oneArrDuration = end1 - start1;
+
+	cout << "OneArrayMult:" << endl << oneArrRes;
+	cout << "duration: " << oneArrDuration.count() << " sec" << endl << endl;
+
+
 	CoordinateMatrix coordMatr1(oneArrMatr1), coordMatr2(oneArrMatr2);
-	cout << "CoordMult:" << endl << coordMatr1.CoordMult(coordMatr2);
+	CoordinateMatrix coordRes(n1, m2);
+
+	auto start2 = chrono::high_resolution_clock::now();
+	coordRes = coordMatr1.CoordMult(coordMatr2);
+	auto end2 = chrono::high_resolution_clock::now();
+	chrono::duration<double> coordDuration = end2 - start2;
+
+	cout << "CoordMult:" << endl << coordRes;
+	cout << "duration: " << coordDuration.count() << " sec" << endl;
 
 	return 0;
 }
