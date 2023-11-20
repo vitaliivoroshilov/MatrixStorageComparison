e3662b975f4bcdcab11ca47323d7919887ac2561
vitaliivoroshilov <vitallizer@yandex.ru>
2023-11-21 00:54:23 +0300

    .

--- MatrixStorageComparison/Main.cpp
+++ MatrixStorageComparison/Main.cpp
@@ -3,17 +3,25 @@
 
 int main()
 {
-	int n1 = 1000, m1 = 1000, n2 = 1000, m2 = 1000, percent = 10;
+	int n1, m1, n2, m2;
+	n1 = m1 = n2 = m2 = 1000;
+	int  percent = 50;
 
 	OneArrayMatrix oneArrMatr1(n1, m1);
 	oneArrMatr1 = generateSparseMatrix(n1, m1, percent);
 	OneArrayMatrix oneArrMatr2(n2, m2);
 	oneArrMatr2 = generateSparseMatrix(n2, m2, percent);
-	cout << "OneArrayMult duration: " << testOneArrayMult(oneArrMatr1, oneArrMatr2) << " sec" << endl << endl;
-
+	double oneArrayDuration = testOneArrayMult(oneArrMatr1, oneArrMatr2);
+	cout << "OneArrayMult duration: " << oneArrayDuration << " sec." << endl;
 
 	CoordinateMatrix coordMatr1(oneArrMatr1), coordMatr2(oneArrMatr2);
-	cout << "CoordMult duration: " << testCoordMult(coordMatr1, coordMatr2) << " sec" << endl << endl;
+	double coordDuration = testCoordMult(coordMatr1, coordMatr2);
+	cout << "CoordMult duration:    " << coordDuration << " sec." << endl;
+
+	if (oneArrayDuration < coordDuration)
+		cout << "OneArrayMult is " << coordDuration / oneArrayDuration << " times faster!";
+	else
+		cout << "CoordMult is " << oneArrayDuration / coordDuration << " times faster!";
 
 	return 0;
 }
--- MatrixStorageComparison/TestMultDuration.cpp
+++ MatrixStorageComparison/TestMultDuration.cpp
@@ -15,8 +15,6 @@ OneArrayMatrix generateSparseMatrix(int n, int m, int percent)
 
 double testOneArrayMult(OneArrayMatrix& matr1, OneArrayMatrix& matr2)
 {
-	OneArrayMatrix matrRes(matr1.n, matr2.m);
-
 	auto start = chrono::high_resolution_clock::now();
 	matr1.OneArrayMult(matr2);
 	auto end = chrono::high_resolution_clock::now();
@@ -27,8 +25,6 @@ double testOneArrayMult(OneArrayMatrix& matr1, OneArrayMatrix& matr2)
 
 double testCoordMult(CoordinateMatrix& matr1, CoordinateMatrix& matr2)
 {
-	CoordinateMatrix matrRes(matr1.n, matr2.m);
-
 	auto start = chrono::high_resolution_clock::now();
 	matr1.CoordMult(matr2);
 	auto end = chrono::high_resolution_clock::now();
